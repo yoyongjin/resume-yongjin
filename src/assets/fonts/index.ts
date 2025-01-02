58541db0afc044420cc9ff197410ff434deb10d0
yoyongjin <yongjinalize@gmail.com>
2025-01-03 01:38:39 +0900

    🔧 pretendard 폰트 next/font/local 설정 및 전역스타일 반영
    - 전역 스타일과 폰트는 tailwind에서 처리하도록 수정

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: './PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  display: 'swap',
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)', ...fontFamily.sans],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
