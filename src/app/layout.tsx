a5098787281436abfcd072e4743e284012dc4e7f
yoyongjin <yongjinalize@gmail.com>
2025-01-03 00:28:57 +0900

    💄 SSR환경에서의 styled-components GlobalStyle 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import StyledComponentsRegistry from '@/lib/styled-registry';
+import GlobalStyle from '@/styles/\bGlobalStyle';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <GlobalStyle />
+          {children}
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
